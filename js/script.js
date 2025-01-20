ecb31bce208f09892b4ce5b778149590ccb99cf4
Jeffrey Laederach <jefflae@outlook.com>
2025-01-20 01:29:44 -0500

    Fixed a bunch of things
    - JavaScript Hamburger Menu
    - Team Member Images
    - Social Media Icons

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-// Close the navbar when a nav-item is clicked 
-document.querySelectorAll('.navbar-nav .nav-link').forEach(item => { 
-    item.addEventListener('click', () => {
-        if (document.querySelector('.navbar-collapse').classList.contains('show')) { 
-            document.querySelector('.navbar-toggler').click(); 
-        } 
+// Close the navbar when a nav-item or dropdown-item is clicked
+document.querySelectorAll('.navbar-nav .nav-link, .dropdown-item').forEach(item => { 
+    item.addEventListener('click', (event) => {
+        // Check if the clicked item is not part of a dropdown toggle
+        if (!event.target.closest('.dropdown-toggle')) {
+            if (document.querySelector('.navbar-collapse').classList.contains('show')) { 
+                document.querySelector('.navbar-toggler').click(); 
+            }
+        }
     }); 
 });
